d86f9f3e89119712175c606af2e17dc29805756c
AntonioFerreira28 <125039990+AntonioFerreira28@users.noreply.github.com>
2023-08-15 09:56:41 -0300

    Add files via upload

---
+++ autocardev/src/app/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return ( //JSX
+    <nav className="bg-cyan-950 p-2 flex gap-3 items-end">
+      <h1 className = "text-3xl text-zinc-100 font-bold uppercase space-y-10" ><i>AUTOCARDEV</i></h1>
+    <ul>
+      <li className= "flex space-x-40">
+          <a href="#" className = "text-zinc-300" ><i>FAVORITOS</i></a>
+          <a href="#" className = "text-zinc-300" ><i>LANÇAMENTOS</i></a>
+          <a href="#" className = "text-zinc-300" ><i>VENDA SEU CARRO</i></a>
+          <a href="#" className = "text-zinc-300" ><i>SOBRE</i></a>
+
+      </li>
+    </ul>
+    </nav>
+  )
+}
